6126b991fcb27c8d241b576a65b1d0967b17351c
Penniug <47276378+Penniug@users.noreply.github.com>
2025-06-09 10:00:25 +0800

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,9 @@
+@echo off
+rem --------------------------------------------------------------------------
+rem
+rem  Gradle start up script for Windows
+rem
+rem --------------------------------------------------------------------------
+
+set DIR=%~dp0
+"%DIR%\gradle\wrapper\gradle-wrapper.jar" %*
